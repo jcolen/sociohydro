d806d5218a7a9a2e96ee1e40a35e02925ccf5ca1
Jonathan Colen <jcolen@odu.edu>
2024-08-20 16:36:04 -0400

    Updated to allow joint loss function

--- param_inference/train_fipy_nn.py
+++ param_inference/train_fipy_nn.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from fipy_dataset import FipyDataset
 from fipy_nn import SociohydroParameterNetwork
@@ -9,9 +10,65 @@ import datetime
 from argparse import ArgumentParser
 from time import time
 
+class JointLossFunction(nn.Module):
+    """ Multi-directional loss function 
+        Loss = MSE(features+growth, target) + \
+            alpha * MSE(features, target) + \
+            beta * MSE(features+SINDy(growth), target)
+        
+        The first term is a simple MSE to improve accuracy
+        The second term encourages the model to do as much as possible without the NN
+        The third term encourages the NN to have a parsimonious representation
+    """
+    def __init__(self, alpha=0., beta=0.):
+        super().__init__()
+
+        self.alpha = alpha
+        self.beta = beta
+
+    def polynomial_library(self, x):
+        """ Return polynomial combinations up to degree 2 
+            x has shape [B, 2, ...]
+            Return something of shape [..., 6]
+        """
+        lib = [
+            torch.ones_like(x[:, 0]),
+            x[:, 0],
+            x[:, 1],
+            x[:, 0]**2,
+            x[:, 1]**2,
+            x[:, 0] * x[:, 1]
+        ]
+        lib = torch.stack(lib, dim=-1)
+        return lib.reshape([-1, lib.shape[-1]])
+    
+    def forward(self, targets, outputs, inputs, features, growth):
+        """ Apply the multi-directional loss function """
+        loss = F.mse_loss(targets, outputs)
+
+        if self.alpha > 0:
+            loss += self.alpha * F.mse_loss(targets, features)
+
+        if self.beta > 0:
+            # Get polynomial feature library, shape [N, 6]
+            A = self.polynomial_library(inputs) # shape [N, 6]
+            b = torch.movedim(growth, 1, -1).reshape([-1, growth.shape[1]]) #shape [N, 2]
+
+            # Solve least squares problem
+            weights = torch.linalg.lstsq(A, b).solution
+
+            # Get least squares predictions
+            growth_pred = A @ weights
+            growth_pred = growth_pred.reshape([growth.shape[0], -1, growth.shape[1]])
+            growth_pred = torch.movedim(growth_pred, -1, 1)
+            loss += self.beta * F.mse_loss(targets, features + growth_pred)
+        
+        return loss
+
 def train(model, 
           train_loader, 
           val_loader, 
+          loss_func,
           optimizer, 
           scheduler, 
           epochs, 
@@ -25,9 +82,11 @@ def train(model,
         # Training loop
         train_loss.append(0)
         for i, batch in enumerate(train_loader):
-            outputs, _ = model(batch['inputs'].to(device), batch['features'].to(device))
+            inputs = batch['inputs'].to(device)
             targets = batch['targets'].to(device)
-            loss = F.mse_loss(outputs, targets)
+            outputs, (features, growth) = model(
+                inputs, batch['features'].to(device))
+            loss = loss_func(targets, outputs, inputs, features, growth)
 
             optimizer.zero_grad()
             loss.backward()
@@ -39,9 +98,11 @@ def train(model,
         val_loss.append(0)
         with torch.no_grad():
             for i, batch in enumerate(val_loader):
-                outputs, _ = model(batch['inputs'].to(device), batch['features'].to(device))
+                inputs = batch['inputs'].to(device)
                 targets = batch['targets'].to(device)
-                loss = F.mse_loss(outputs, targets)
+                outputs, (features, growth) = model(
+                    inputs, batch['features'].to(device))
+                loss = loss_func(targets, outputs, inputs, features, growth)
                 val_loss[epoch] += loss.item() / len(val_loader)
 
         # Bookkeeping
@@ -69,6 +130,8 @@ if __name__ == '__main__':
     parser.add_argument('--base_lr', type=float, default=3e-4)
     parser.add_argument('--scheduler_step', type=float, default=0.98)
     parser.add_argument('--val_split', type=float, default=0.5)
+    parser.add_argument('--alpha', type=float, default=1.)
+    parser.add_argument('--beta', type=float, default=0.)
     args = parser.parse_args()
     
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -78,6 +141,7 @@ if __name__ == '__main__':
     model = SociohydroParameterNetwork(grid=args.grid).to(device)
 
     # Initialize optimizer and learning rate scheduler
+    loss_func = JointLossFunction(alpha=args.alpha, beta=args.beta)
     optimizer = torch.optim.Adam([
         {'params': model.coefs, 'lr': args.coef_lr},
         {'params': model.local_network.parameters()}
@@ -110,6 +174,7 @@ if __name__ == '__main__':
         model=model, 
         train_loader=train_loader, 
         val_loader=val_loader,
+        loss_func=loss_func,
         optimizer=optimizer,
         scheduler=scheduler,
         epochs=args.epochs, 
